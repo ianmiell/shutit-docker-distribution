ed4968a97894ac4f6aa83d047c00cfeb4a00134e
Ian Miell <ian.miell@gmail.com>
2016-11-26 10:06:11 +0000

    initial

---
+++ shutit_docker_distribution.py
@@ -0,0 +1,83 @@
+import random
+import string
+import os
+import inspect
+
+from shutit_module import ShutItModule
+
+class shutit_docker_distribution(ShutItModule):
+
+
+	def build(self, shutit):
+		vagrant_image = shutit.cfg[self.module_id]['vagrant_image']
+		vagrant_provider = shutit.cfg[self.module_id]['vagrant_provider']
+		gui = shutit.cfg[self.module_id]['gui']
+		memory = shutit.cfg[self.module_id]['memory']
+		run_dir = os.path.dirname(os.path.abspath(inspect.getsourcefile(lambda:0))) + '/vagrant_run'
+		module_name = 'shutit_docker_distribution_' + ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(6))
+		shutit.send('command rm -rf ' + run_dir + '/' + module_name + ' && command mkdir -p ' + run_dir + '/' + module_name + ' && command cd ' + run_dir + '/' + module_name)
+		shutit.send('vagrant init ' + vagrant_image)
+		shutit.send_file(run_dir + '/' + module_name + '/Vagrantfile','''
+Vagrant.configure(2) do |config|
+  config.vm.box = "''' + vagrant_image + '''"
+  # config.vm.box_check_update = false
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  # config.vm.network "private_network", ip: "192.168.33.10"
+  # config.vm.network "public_network"
+  # config.vm.synced_folder "../data", "/vagrant_data"
+  config.vm.provider "virtualbox" do |vb|
+    vb.gui = ''' + gui + '''
+    vb.memory = "''' + memory + '''"
+    vb.name = "shutit_docker_distribution"
+  end
+end''')
+		pw = shutit.get_env_pass()
+		try:
+			shutit.multisend('vagrant up --provider ' + shutit.cfg['shutit-library.virtualization.virtualization.virtualization']['virt_method'],{'assword for':pw},timeout=99999)
+		except:
+			shutit.multisend('vagrant up',{'assword for':pw},timeout=99999)
+		shutit.login(command='vagrant ssh')
+		shutit.login(command='sudo su -',password='vagrant')
+
+
+
+		shutit.logout()
+		shutit.logout()
+		return True
+
+	def get_config(self, shutit):
+		shutit.get_config(self.module_id,'vagrant_image',default='ubuntu/trusty64')
+		shutit.get_config(self.module_id,'vagrant_provider',default='virtualbox')
+		shutit.get_config(self.module_id,'gui',default='false')
+		shutit.get_config(self.module_id,'memory',default='1024')
+
+		return True
+
+	def test(self, shutit):
+
+		return True
+
+	def finalize(self, shutit):
+
+		return True
+
+	def isinstalled(self, shutit):
+
+		return False
+
+	def start(self, shutit):
+
+		return True
+
+	def stop(self, shutit):
+
+		return True
+
+def module():
+	return shutit_docker_distribution(
+		'imiell.shutit_docker_distribution.shutit_docker_distribution', 1644823336.0001,
+		description='',
+		maintainer='',
+		delivery_methods=['bash'],
+		depends=['shutit.tk.setup','shutit-library.virtualbox.virtualbox.virtualbox','tk.shutit.vagrant.vagrant.vagrant']
+	)
